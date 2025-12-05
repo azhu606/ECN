1d9e528c7691187eb10a20bae5bfcdc654c6261f
raghav-agarwalla-104932 <raghav2761@gmail.com>
2025-12-05 00:11:50 -0500

    test changes

--- ECN_Backend/app.py
+++ ECN_Backend/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from routes import api_bp  # your blueprint file
 from db_ops import create_all  # helper to create tables
+import os
 
 def create_app():
     app = Flask(__name__)
 
     # PostgreSQL connection (no password)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres@127.0.0.1:5432/ecn"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://neondb_owner:npg_HD07lOxEZWik@ep-muddy-flower-ahr3u2zq-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # Register blueprints
